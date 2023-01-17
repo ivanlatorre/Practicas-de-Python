df017004b3bb94bafa371f31edc4ed44293eea57
ivan <ivanlatorre720@gmail.com>
2023-01-17 15:13:17 -0300

    tipados

---
+++ TipoDatos.py
@@ -0,0 +1,11 @@
+Num1 = 120
+Num2 = 135.45   
+Texto = "Curso de Aprendizaje"
+Nombre = "Ivan"
+print(type(Num1))
+print(type(Num2))
+print(type(Texto))
+print(type(Nombre))
+Estado = True
+print(type(Estado))
+print(type(num1))#error
