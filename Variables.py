143cf101575760075f4f6922a1f765d665636e17
ivan <ivanlatorre720@gmail.com>
2023-01-17 15:07:25 -0300

    variables

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#-*- coding: utf:-8 -*-
+
+
+nombre = "Ivan"
+print(nombre)
+
+Sueldo = 1025.45
+print(Sueldo)
+
+
+Contenido="Curso de aprendisaje"
+print(Contenido)
+
+Contenido=250.35
+print(Contenido)
