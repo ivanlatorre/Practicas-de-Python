e316cd3593b2164c0307fa4ef9d0d8e39b271801
ivan <ivanlatorre720@gmail.com>
2023-01-18 02:10:03 -0300

    concatenacion

---
+++ Projet-Python/tareas/concatenacion.py
@@ -0,0 +1,10 @@
+Nombre = "Ivan"
+Apellido = "Latorre"
+Especialidad = "Desarrollador de Sistemas"
+#1era p
+print(Nombre +" "+ Apellido)
+#2da forma concatenación
+print("Mi nombre es: %s" %Nombre)
+print("Mi nombre es: %s y Mi Apellido: %s " %(Nombre,Apellido) )
+#3era forma concatenacion
+print("Mi nombre es: {0} y mi especialidad es: {1} ".format(Nombre,Especialidad) )#listas
