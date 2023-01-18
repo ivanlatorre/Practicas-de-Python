904945b80e3957f29d66315551d2c1e908708361
ivan <ivanlatorre720@gmail.com>
2023-01-18 00:41:25 -0300

    conversione y comentarios

---
+++ Projet-Python/tareas/comentarios.py
@@ -0,0 +1,13 @@
+#comentarios o referencias del codifo
+""" Comentario Multi linead """
+#**
+# comentario
+# en
+# varias
+# numerales
+# #
+""" Cuso de Aprendisaje de Python desde Cero
+ Nivel Básico
+ Nivel Intermedio
+ Nivel Avanzado
+ """
--- Projet-Python/tareas/conversiones.py
+++ Projet-Python/tareas/conversiones.py
@@ -9,3 +9,17 @@ print(num2)
 print(num3)
 print(sueldo)
 print(texto)
+print(type(texto))
+print(type(num1))
+print(type(num3))
+#int (x) Convierte x datos a entero
+#float (x) Convierte x en un número de punto flotante.
+#str (x) Convierte x a una cadena.
+# hex (x) Convierte x entero en una cadena hexadecimal.
+print(num1+num2)#se concatenan
+print(int(num1)+int(num2))#se suman string en su valor a numero entero
+print(int(sueldo))#no considera el dato decimal
+print("Sueldo Mensual: " ,sueldo) #no se esta convinando  error datos diferentes sin la coma
+print("Sueldo Mensual: "+str(sueldo))#convierte el entero en string
+print(float(num3))#entero a decimal
+print(hex(num3))#hexadecimal a un entero
