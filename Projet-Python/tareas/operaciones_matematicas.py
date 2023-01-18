2b2b2abdc1eef26d59c3b506105574361f951ee6
ivan <ivanlatorre720@gmail.com>
2023-01-18 01:06:48 -0300

    operaciones matemáticas

---
+++ Projet-Python/tareas/operaciones_matematicas.py
@@ -0,0 +1,37 @@
+"""
+Operaciones Matemáticas con Python
+python nos permite realizar los siguientes operaciones matemáticas,
+A continuación presentamos cada una con su  operador respectivo:
+
+Suma +
+Resta -
+Multiplicación *
+División /
+División entera //
+Módulo (resto) %
+Potenciación **
+"""
+num1 = 150
+num2 = 200
+print("--- Suma ---")
+resultado_suma = num1+num2
+print( "Resultado suma ",resultado_suma)
+print("--- Resta ---")
+resultado_resta = num1-num2
+print("Resultado resta",resultado_resta)
+print("--- Multiplicación ---")
+resultado_multiplica = num1 * num2
+print("Resultado Multiplica ",resultado_multiplica)
+num3 = 1035
+num4 = 100
+print("--- División ---")
+resultado_division = num3/num4
+print("Resultado división ", resultado_division)
+resultado_division_exacta = num3//num4
+print("Resultado división exacta ",resultado_division_exacta)
+resultado_division_residuo = num3 % num4
+print("Resultado división residuo",resultado_division_residuo)
+print("--- Potencia ---")
+print("Potenciación", 5**2)
+
+
