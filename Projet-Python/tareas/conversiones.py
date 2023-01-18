31b5710c166fd7c665d723221fd68bc850edfbce
ivan <ivanlatorre720@gmail.com>
2023-01-18 00:25:07 -0300

    conversiones

---
+++ Projet-Python/tareas/conversiones.py
@@ -0,0 +1,11 @@
+num1 = "120"
+num2 = "150"
+print(int(num1)+int(num2))
+num3 = 103
+sueldo = 1035.78
+texto = "Curso de Aprendizaje"
+print(num1)
+print(num2)
+print(num3)
+print(sueldo)
+print(texto)
