7fceb20c815bb43dbc88336a2347297b3270961c
ivan <ivanlatorre720@gmail.com>
2023-01-19 01:19:04 -0300

    metodos y cadenas

---
+++ Projet-Python/tareas/metodos_cadenas.py
@@ -0,0 +1,5 @@
+cadena = " Estoy aprendiendo el lenguaje de programmación de Python"
+print(cadena)
+print("--- Buscar infomacion en la cadena ---")
+print("lengiuaje esta en posición: ",cadena.find("lenguaje"))
+print("Estoy esta en la posición: ", cadena.find(("Estoy")))
---
+++ Projet-Python/tareas/operadores de cadenas.py
@@ -0,0 +1,11 @@
+cadena1 = "Cursos de Aprendizaje"
+cadena2 = "Python " *5 # multiplica el contenido de la cadena
+cadena3 = "Lenguaje de Programación"
+
+Resultado = cadena1+ " " + cadena2
+print(Resultado)
+
+print("------------------------")
+cadena4 = "Cursos"
+cadena4 += " de Programación"#+= juntos
+print(cadena4)
