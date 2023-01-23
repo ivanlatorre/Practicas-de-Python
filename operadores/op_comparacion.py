d3e822a4485ae6ce29bd05383006702d78845d88
ivan <ivanlatorre720@gmail.com>
2023-01-23 14:18:30 -0300

    comparacion

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,20 @@
+es_igual_que = 5 == 6
+distinto_de =  5 != 6
+mayor_que = 5 > 6
+menor_que =  5 < 6
+mayor_o_igual = 5 >= 6
+menor_o_igual = 5 <= 6
+
+print("5 es igual a 6 // 5 = 6: ",es_igual_que)
+print("5 es distinque que 6 // 5 != 6: ",distinto_de)
+print("5 es mayor que 6 // 5 > 6: ",mayor_que)
+print("5 es menor que 6 // 5 < 6: ",menor_que)
+print("5 es mayor o igual que 6 // 5 >= 6: ",mayor_o_igual)
+print("5 es menor o igual que 6 // 5 <= 6: ",menor_o_igual)
+
+#calculos combinados
+a = 5
+b = 10
+c = 20
+comparacion = a + b < c
+print("a + b < c: ",comparacion)
