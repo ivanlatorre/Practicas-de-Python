97524ae73b807c8b6cdfd16345477e8aef790093
ivan <ivanlatorre720@gmail.com>
2023-01-24 00:20:29 -0300

    if elif else

---
+++ condicionales/if-elif-else.py
@@ -0,0 +1,28 @@
+ingreso_mensual = 100000
+print(f"ingreso es de: {ingreso_mensual} ")
+if ingreso_mensual > 10000:
+    print("estas bien en cualquier parte del mundo")
+elif ingreso_mensual > 10000:
+    print("estas bien en latinoamérica")
+else:
+    print("sos pobre")
+
+
+ingreso_mensual = 1001
+print(f"ingreso es de: {ingreso_mensual} ")
+if ingreso_mensual > 10000:
+    print("estas bien en cualquier parte del mundo")
+elif ingreso_mensual > 1000:
+    print("estas bien en latinoamérica")
+else:
+    print("sos pobre")
+    
+ingreso_mensual = 500
+print(f"ingreso es de: {ingreso_mensual} ")
+if ingreso_mensual > 10000:
+    print("estas bien en cualquier parte del mundo")
+elif ingreso_mensual > 1000:
+    print("estas bien en latinoamérica")
+else:
+    print("sos pobre") 
+    
---
+++ condicionales/if-else.py
@@ -0,0 +1,7 @@
+contraseña_almacenada = "lobito"
+contraseña_escrita = "popina"
+if contraseña_almacenada == contraseña_escrita:
+    print("INICIANDO SESIÓN ...")
+else:
+    print("Contraseña erronea, intnete de nuevo")
+print("no forma parte de ninguna condicion")
