2a2396fc21aa98248ecb71a2ea48c6fa77e4ca28
ivan <ivanlatorre720@gmail.com>
2023-01-30 21:52:27 -0300

    inputs float

--- input/inputs.py
+++ input/inputs.py
@@ -5,6 +5,13 @@ print(f"el nombre es: {nombre} ")
 numero =  input("dame un numero: ")
 resultado = numero*2 #duplica 
 print("duplica el texto",resultado)
+numero2 = int(input("numero2: "))#convierte numero a entero
+resultado2 = numero2*2
+print("numero2: ",resultado2)
+#float
+numero_flotante = float(input("valor flotante: "))
+resultado_float = numero_flotante*2
+print("numero float: ",resultado_float)
 
 
 
