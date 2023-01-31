30be0c25077979369a69fd7599ad2ae0370cf892
ivan <ivanlatorre720@gmail.com>
2023-01-30 21:39:36 -0300

    inputs

---
+++ input/inputs.py
@@ -0,0 +1,12 @@
+#input pedir un dato al usuario
+nombre = input("Dame tu nombre: ")#siempre devuelve un texto
+#mostrando el dato
+print(f"el nombre es: {nombre} ")
+numero =  input("dame un numero: ")
+resultado = numero*2 #duplica 
+print("duplica el texto",resultado)
+
+
+
+
+
